16dfcd28b892f1ca4be62ed60c8d9c5b7b8e9763
lisboamari <74574458+lisboamari@users.noreply.github.com>
2024-06-07 16:31:53 -0300

    Adicionando arquivo de código para a atividade
    Código simples de Star Wars Day

---
+++ starWars.py
@@ -0,0 +1,11 @@
+# A simple Python code for celebrating 2024's Star Wars Day
+
+from datetime import date
+
+starWarsDay = date(2024,5,4);
+todayDate = date.today();
+
+if (todayDate == starWarsDay):
+  print ("May the 4th be with you")
+else:
+  print ("May the force be with you")
